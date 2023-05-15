0f23b392b2aa84b136a4441c8bdd0471bf952752
Carlos Andres <contacto@carlosaperez.me>
2023-05-15 13:04:56 -0500

    Docker-mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ServiceHelloWorld } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ServiceHelloWorld } from './app.service';
+import { AppController } from './app.controller';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname, '../', 'public'),
     }),
     PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+     ConfigModule.forRoot()
   ],
   controllers: [AppController],
   providers: [ServiceHelloWorld],
